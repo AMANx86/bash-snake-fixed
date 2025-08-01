a3a0f837677507f5236ed539534002197347c139
AMANx86 <118564769+AMANx86@users.noreply.github.com>
2025-08-02 02:33:11 +0600

    Update snakeupdate.sh
    The arrow key now works and can be played in termux there is a Retry and Exit function now.

--- snake.sh
+++ snake.sh
@@ -2,35 +2,35 @@
 
 IFS=''
 
+# Terminal dimensions for the game area
 declare -i height=$(($(tput lines)-5)) width=$(($(tput cols)-2))
 
-# row and column number of head
+# Snake state
 declare -i head_r head_c tail_r tail_c
-
-declare -i alive  
+declare -i alive
 declare -i length
 declare body
-
 declare -i direction delta_dir
-declare -i score=0
+declare -i score
 
+# Colors
 border_color="\e[30;43m"
 snake_color="\e[32;42m"
 food_color="\e[34;44m"
 text_color="\e[31;43m"
 no_color="\e[0m"
 
-# signals
+# Direction arrays: 0=up, 1=right, 2=down, 3=left
+move_r=([0]=-1 [1]=0 [2]=1 [3]=0)
+move_c=([0]=0 [1]=1 [2]=0 [3]=-1)
+
+# Signals (for process communication)
 SIG_UP=USR1
 SIG_RIGHT=USR2
 SIG_DOWN=URG
 SIG_LEFT=IO
 SIG_QUIT=WINCH
 SIG_DEAD=HUP
-
-# direction arrays: 0=up, 1=right, 2=down, 3=left
-move_r=([0]=-1 [1]=0 [2]=1 [3]=0)
-move_c=([0]=0 [1]=1 [2]=0 [3]=-1)
 
 init_game() {
     clear
@@ -47,7 +47,6 @@ move_and_draw() {
     echo -ne "\e[${1};${2}H$3"
 }
 
-# print everything in the buffer
 draw_board() {
     move_and_draw 1 1 "$border_color+$no_color"
     for ((i=2; i<=width+1; i++)); do
@@ -70,113 +69,87 @@ draw_board() {
     echo
 }
 
-# set the snake's initial state
 init_snake() {
     alive=0
     length=10
     direction=0
     delta_dir=-1
-
+    score=0
     head_r=$((height/2-2))
     head_c=$((width/2))
-
     body=''
     for ((i=0; i<length-1; i++)); do
         body="1$body"
     done
-
     local p=$((${move_r[1]} * (length-1)))
     local q=$((${move_c[1]} * (length-1)))
-
     tail_r=$((head_r+p))
     tail_c=$((head_c+q))
-
     eval "arr$head_r[$head_c]=\"${snake_color}o$no_color\""
-
     prev_r=$head_r
     prev_c=$head_c
-
     b=$body
     while [ -n "$b" ]; do
-        # change in each direction
         local p=${move_r[$(echo $b | grep -o '^[0-3]')]}
         local q=${move_c[$(echo $b | grep -o '^[0-3]')]}
-
         new_r=$((prev_r+p))
         new_c=$((prev_c+q))
-
         eval "arr$new_r[$new_c]=\"${snake_color}o$no_color\""
-
         prev_r=$new_r
         prev_c=$new_c
-
         b=${b#[0-3]}
     done
 }
 
 is_dead() {
     if [ "$1" -lt 0 ] || [ "$1" -ge "$height" ] || \
-        [ "$2" -lt 0 ] || [ "$2" -ge "$width" ]; then
+       [ "$2" -lt 0 ] || [ "$2" -ge "$width" ]; then
         return 0
     fi
-
     eval "local pos=\${arr$1[$2]}"
-
     if [ "$pos" == "${snake_color}o$no_color" ]; then
         return 0
     fi
-
     return 1
 }
 
 give_food() {
-    local food_r=$((RANDOM % height))
-    local food_c=$((RANDOM % width))
-    eval "local pos=\${arr$food_r[$food_c]}"
-
-    while [ "$pos" != ' ' ]; do
+    local food_r food_c pos
+    while true; do
         food_r=$((RANDOM % height))
         food_c=$((RANDOM % width))
         eval "pos=\${arr$food_r[$food_c]}"
+        if [ "$pos" = " " ]; then
+            break
+        fi
     done
-
     eval "arr$food_r[$food_c]=\"$food_color@$no_color\""
 }
 
 move_snake() {
     local newhead_r=$((head_r + move_r[direction]))
     local newhead_c=$((head_c + move_c[direction]))
-
     eval "local pos=\${arr$newhead_r[$newhead_c]}"
-
-    if $(is_dead $newhead_r $newhead_c); then
+    if is_dead $newhead_r $newhead_c; then
         alive=1
         return
     fi
-
     if [ "$pos" == "$food_color@$no_color" ]; then
         length+=1
         eval "arr$newhead_r[$newhead_c]=\"${snake_color}o$no_color\""
         body="$(((direction+2)%4))$body"
         head_r=$newhead_r
         head_c=$newhead_c
-
         score+=1
-        give_food;
+        give_food
         return
     fi
-
     head_r=$newhead_r
     head_c=$newhead_c
-
     local d=$(echo $body | grep -o '[0-3]$')
-
     body="$(((direction+2)%4))${body%[0-3]}"
-
     eval "arr$tail_r[$tail_c]=' '"
     eval "arr$head_r[$head_c]=\"${snake_color}o$no_color\""
-
-    # new tail
     local p=${move_r[(d+2)%4]}
     local q=${move_c[(d+2)%4]}
     tail_r=$((tail_r+p))
@@ -192,23 +165,27 @@ change_dir() {
 
 getchar() {
     trap "" SIGINT SIGQUIT
-    trap "return;" $SIG_DEAD
-
+    trap "return;" SIGTERM
     while true; do
-        read -s -n 1 key
-        case "$key" in
-            [qQ]) kill -$SIG_QUIT $game_pid
-                  return
-                  ;;
-            [kK]) kill -$SIG_UP $game_pid
-                  ;;
-            [lL]) kill -$SIG_RIGHT $game_pid
-                  ;;
-            [jJ]) kill -$SIG_DOWN $game_pid
-                  ;;
-            [hH]) kill -$SIG_LEFT $game_pid
-                  ;;
-       esac
+        read -rsn1 key
+        if [[ "$key" == $'\e' ]]; then
+            read -rsn2 rest
+            key+="$rest"
+            case "$key" in
+                $'\e[A') if kill -0 $game_pid 2>/dev/null; then kill -$SIG_UP $game_pid; fi ;;
+                $'\e[B') if kill -0 $game_pid 2>/dev/null; then kill -$SIG_DOWN $game_pid; fi ;;
+                $'\e[C') if kill -0 $game_pid 2>/dev/null; then kill -$SIG_RIGHT $game_pid; fi ;;
+                $'\e[D') if kill -0 $game_pid 2>/dev/null; then kill -$SIG_LEFT $game_pid; fi ;;
+            esac
+        else
+            case "$key" in
+                [qQ]) if kill -0 $game_pid 2>/dev/null; then kill -$SIG_QUIT $game_pid; fi; return ;;
+                [kK]) if kill -0 $game_pid 2>/dev/null; then kill -$SIG_UP $game_pid; fi ;;
+                [lL]) if kill -0 $game_pid 2>/dev/null; then kill -$SIG_RIGHT $game_pid; fi ;;
+                [jJ]) if kill -0 $game_pid 2>/dev/null; then kill -$SIG_DOWN $game_pid; fi ;;
+                [hH]) if kill -0 $game_pid 2>/dev/null; then kill -$SIG_LEFT $game_pid; fi ;;
+            esac
+        fi
     done
 }
 
@@ -217,11 +194,9 @@ game_loop() {
     trap "delta_dir=1;" $SIG_RIGHT
     trap "delta_dir=2;" $SIG_DOWN
     trap "delta_dir=3;" $SIG_LEFT
-    trap "exit 1;" $SIG_QUIT
-
+    trap "alive=2;" $SIG_QUIT
     while [ "$alive" -eq 0 ]; do
         echo -e "\n${text_color}           Your score: $score $no_color"
-
         if [ "$delta_dir" -ne -1 ]; then
             change_dir $delta_dir
         fi
@@ -229,11 +204,9 @@ game_loop() {
         draw_board
         sleep 0.03
     done
-    
+    # game over
     echo -e "${text_color}Oh, No! You 0xdead$no_color"
-
-    # signals the input loop that the snake is dead
-    kill -$SIG_DEAD $$
+    kill -TERM $$
 }
 
 clear_game() {
@@ -241,14 +214,29 @@ clear_game() {
     echo -e "\e[?25h"
 }
 
-init_game
-init_snake
-give_food
-draw_board
+main() {
+    while true; do
+        init_game
+        init_snake
+        give_food
+        draw_board
+        game_loop &
+        game_pid=$!
+        getchar
+        clear_game
+        # Retry/Exit prompt
+        while true; do
+            echo -ne "${text_color}Retry (R) or Exit (E)? $no_color"
+            read -n1 key
+            echo
+            case "$key" in
+                [Rr]) break ;; # Retry outer loop
+                [Ee]) exit 0 ;; # Exit script
+                *) echo "Please press R or E." ;;
+            esac
+        done
+    done
+}
 
-game_loop &
-game_pid=$!
-getchar
-
-clear_game
+main
 exit 0
